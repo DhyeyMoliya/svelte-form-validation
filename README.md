b800a9e86a5d18e02d5604479e027dfbb1a67b32
Dhyey Moliya <dhyeymoliya@gmail.com>
2021-09-23 16:44:23 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,322 @@
+# Welcome to svelte-form-validation 👋
+
+[![NPM Version](https://img.shields.io/npm/v/svelte-form-validation.svg?orange=blue)](https://npmjs.org/package/svelte-form-validation)
+
+> Svelte Form Validation Library
+
+### 🏠 [Homepage](https://github.com/DhyeyMoliya/svelte-form-validation#readme)
+
+## Install
+
+```sh
+npm install svelte-form-validation
+```
+
+> This package uses a popular Schema Validation library **[Yup](https://github.com/jquense/yup)**. For documentation on how to create Validation Schema, checkout Yup's official Github Repo https://github.com/jquense/yup.
+
+## Basic Usage Example
+
+_[demo/Basic.svelte](https://github.com/DhyeyMoliya/svelte-form-validation/blob/master/demo/Basic.svelte)_ :
+
+```html
+<script>
+	import { createForm, Schema } from 'svelte-form-validation';
+
+	// Create Form Instance
+	const { values, formControl, isValid } = createForm({
+		// Initial Form Data
+		initialValues: {
+			email: '',
+			password: ''
+		},
+		// Form Validation using Yup Schema
+		validationSchema: Schema.object().shape({
+			email: Schema.string().email().required(),
+			password: Schema.string().min(6).required()
+		})
+	});
+
+	const onSubmit = () => {
+		// "$values" contains current Form Data
+		console.log($values);
+	};
+</script>
+
+<form on:submit|preventDefault="{onSubmit}">
+	<input type="text" name="email" bind:value="{$values.email}" use:formControl />
+	<input type="password" name="password" bind:value="{$values.password}" use:formControl />
+
+	<button type="submit" disabled="{!$isValid}">Submit</button>
+</form>
+```
+
+## Full Usage Example
+
+_[demo/Full.svelte](https://github.com/DhyeyMoliya/svelte-form-validation/blob/master/demo/Full.svelte)_ :
+
+```html
+<script lang="ts">
+  import { createForm, Schema } from "svelte-form-validation";
+  import UserAddressForm from "./UserAddressForm.svelte"; // Components
+
+  // (Optional) Form's Data type will be automatically inferred from "initialValues" in "createForm" if type of Data is not specified
+  type FormData = {
+    title: string,
+    description: string,
+    coverImage: FileList,
+    users: {
+      name: string,
+      email: string,
+      address: {
+        state: string,
+        city: string,
+      },
+    }[],
+  };
+
+  // Create Form Instance
+  const {
+    values, // Svelte Store<FormData> containing Form Data
+    state, // Svelte Store<FormState> containing Form State - { [every_property]: { _touched: boolean, _errors: string[] }}
+    isValid, // Svelte Store<boolean> containing entire Form's validation status
+    isTouched, // Svelte Store<boolean> containing entire Form's touched status
+    validateForm, // Function(highlight: 'none' | 'errors' | 'all' = 'none') for manually validting entire form
+    handleChange, // Function(event: Event) to manually updating individual form control's state - can be used in place of "formControl" Action
+    setTouched, // Function() for manually setting Form state as "touched"
+    updateForm, // Function() for updating Form's Structure after Form Controls are Added or Removed in cases like Form Arrays
+    formControl, // Svelte Action to be used with <input>, <select>, <textarea> or similar HTML input elements
+    resetForm, // Reset the Form with optional new value and clear validation
+  } = createForm<FormData>({
+    // Initial Values of Form
+    initialValues: {
+      title: "", // Simple String
+      description: "", // Simple String
+      coverImage: "", // File Input
+      users: [], // Complex Form Array
+    },
+    // Validation Schema (Yup - https://github.com/jquense/yup)
+    validationSchema: Schema.object().shape({
+      title: Schema.string().min(8).required(),
+      description: Schema.string(),
+      coverImage: Schema.mixed().test(value => value?.length > 0), // Custom validation because yup does not suport file objects
+      users: Schema.array().of({
+        name: Schema.string().required(),
+        email: Schema.string().email().required(),
+        address: Schema.object().shape({
+          state: Schema.string().required(),
+          city: Schema.string(),
+        }),
+      }),
+    }),
+    // CSS class validations
+    css: {
+      enabled: true, // use CSS classes or not
+      validClass: "is-valid", // CSS class added to valid form controls
+      invalidClass: "is-invalid", // CSS class added to invalid form controls
+      useValid: true, // Add CSS classes to valid form controls
+      useInvalid: true, // Add CSS classes to invalid form controls
+    },
+    validateOnChange: true, // Whether to validate on "change" event of element and form value change
+    validateOnBlur: true, // Whether to validate on "blur" event of element
+  });
+
+  // Add new user to Users Form Array
+  const addUser = () => {
+    // Update Form Data
+    $values.users = [
+      ...$values.users,
+      {
+        name: "",
+        email: "",
+        address: {
+          state: "",
+          city: "",
+        },
+      },
+    ];
+    updateForm(); // Manually trigger Form Update - Required
+  };
+
+  // Remove user from Users Form Array
+  const removeUser = (index) => () => {
+    $values.users = $values.users.filter((_, i) => i !== index); // Update Form Data
+    $state.users = $state.users.filter((_, i) => i !== index); // Updating State is required after removing Form Controls
+    updateForm(); // Manually trigger Form Update - Required
+  };
+
+  // Submit Form
+  const onSubmit = () => {
+    console.log($values); // Get Form Data
+    // Reset form after submit
+    resetForm({
+      title: "",
+      description: "",
+      coverImage: "",
+      users: [],
+    });
+  };
+
+  $: console.log($values, $state); // Log Form Data and Form State on every Change
+</script>
+
+<form on:submit|preventDefault={onSubmit}>
+
+    <input
+        placeholder="Title"
+        name="title"
+        bind:value={$values.title}
+        use:formControl
+    />
+    {#if $state.title._errors?.length}
+        {#each $state.title._errors as error}
+            <span class="error">{error}</span>
+        {/each}
+    {/if}
+
+    <input
+        placeholder="Description"
+        name="description"
+        bind:value={$values.description}
+        use:formControl
+    />
+    {#if $state.description._errors?.length}
+        {#each $state.description._errors as error}
+            <span class="error">{error}</span>
+        {/each}
+    {/if}
+
+    <input
+        name="coverImage"
+        accept="image/*"
+        bind:files={$values.coverImage}
+        use:formControl
+    />
+    {#if $state.coverImage._errors?.length}
+        {#each $state.coverImage._errors as error}
+        <span class="error">{error}</span>
+        {/each}
+    {/if}
+
+    {#if $values.coverImage?.length}
+        <div class="image-preview">
+            <img
+            src={URL.createObjectURL($values.coverImage[0])}
+            alt="Cover"
+            height="150" />
+        </div>
+    {/if}
+
+    {#each $values.users as user, index}
+        <h2>
+            User {user.name}
+            <button type="button" on:click={removeUser(i)}>
+                Remove User
+            </button>
+        </h2>
+
+        <input
+            placeholder="name"
+            name="users[{index}].name"
+            bind:value={user.name}
+            use:formControl
+        />
+        {#if $state.users[index].name._errors?.length}
+            {#each $state.users[index].name._errors as error}
+                <span class="error">{error}</span>
+            {/each}
+        {/if}
+
+        <input
+            placeholder="email"
+            name="users[{index}].email"
+            bind:value={user.email}
+            use:formControl
+        />
+        {#if $state.users[index].email._errors?.length}
+            {#each $state.users[index].email._errors as error}
+                <span class="error">{error}</span>
+            {/each}
+        {/if}
+
+        <!-- Using with Components -->
+        <UserAddressForm {form} {state} {formControl} {index} />
+
+    {/each}
+
+    <button type="button" on:click={addUser}>
+        Add User
+    </button>
+
+    <button type="button" on:click={() => validateForm('errors')}>
+        Validate Form
+    </button>
+
+    <button type="submit" disabled={!$isValid}>
+        Submit
+    </button>
+
+</form>
+
+<style>
+    .valid {
+        border: 1px solid green;
+    }
+
+    .invalid {
+        border: 1px solid red;
+    }
+
+    .error {
+        color: red;
+    }
+</style>
+```
+
+_[demo/UserAddressForm.svelte](https://github.com/DhyeyMoliya/svelte-form-validation/blob/master/demo/UserAddressForm.svelte)_ :
+
+```html
+<script lang="ts">
+	export let form: any;
+	export let state: any;
+	export let formControl;
+	export let index: number;
+</script>
+
+<input
+	type="text"
+	placeholder="State"
+	bind:value="{$values.users[index].address.state}"
+	name="users[{index}].address.state"
+	use:formControl
+/>
+{#if $state.users[index].address.state._errors?.length} {#each
+$state.users[index].address.state._errors as error}
+<span class="error">{error}</span>
+{/each} {/if}
+
+<input
+	type="text"
+	placeholder="City"
+	bind:value="{$values.users[index].address.city}"
+	name="users[{index}].address.city"
+	use:formControl
+/>
+{#if $state.users[index].address.city._errors?.length} {#each
+$state.users[index].address.city._errors as error}
+<span class="error">{error}</span>
+{/each} {/if}
+```
+
+## 🤝 Contributing
+
+Contributions, issues and feature requests are welcome!
+
+Feel free to check [issues page](https://github.com/DhyeyMoliya/svelte-form-validation/issues).
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
+
+## 📝 License
+
+This project is [MIT](https://github.com/DhyeyMoliya/svelte-form-validation/blob/master/LICENSE.md) licensed.
---
+++ src/lib/index.ts
@@ -0,0 +1,357 @@
+import type { Writable, Unsubscriber } from 'svelte/store';
+import { get, writable } from 'svelte/store';
+import type { ObjectSchema, ValidationError } from 'yup';
+
+type FormState = {
+    [key: string]: {
+        _touched?: boolean,
+        _errors?: string[],
+        [key: string]: any,
+    }
+};
+
+type FormConfigInput<Data = any> = {
+    initialValues: Data,
+    validationSchema?: ObjectSchema<any>,
+    css?: {
+        enabled?: boolean,
+        validClass?: string,
+        invalidClass?: string,
+        useValid?: boolean,
+        useInvalid?: boolean,
+    }
+    validateOnChange?: boolean,
+    validateOnBlur?: boolean,
+};
+
+function isNullish(value) {
+    return value === undefined || value === null;
+}
+
+function isEmpty(object) {
+    return isNullish(object) || Object.keys(object).length <= 0;
+}
+
+const createState = (initialValues: any, state: Writable<FormState>, validationSchema: ObjectSchema<any>) => {
+    if (validationSchema) {
+        state.set(createValidatedState(initialValues, validationSchema.fields));
+    } else {
+        state.set(createRawState(initialValues));
+    }
+}
+
+const createValidatedState = <Data = any>(data: Data, schema: ObjectSchema<any>, state: FormState = {}) => {
+    for (let key in schema) {
+        if (schema[key].type === 'array') {
+            const values = ((data?.[key] || []) as any[]);
+            state[key] = values.map(
+                (value, index) => createValidatedState(value, schema[key].innerType.fields, Object.assign({}, state[key]?.[index]))
+            );
+            state[key]._errors = [];
+        } else if (schema[key].type === 'object' && !isEmpty(schema[key].fields)) {
+            state[key] = {
+                ...createValidatedState(data[key], schema[key].fields, Object.assign({}, state[key])),
+                _errors: [],
+            };
+        } else {
+            state[key] = {
+                _touched: state?.[key]?._touched || false,
+                _errors: [],
+            };
+        }
+    }
+    return state;
+}
+
+const createRawState = <Data>(data: Data, state: FormState = {}): FormState => {
+    for (let key in data) {
+        if (Array.isArray(data[key])) {
+            const values = (data[key] || []) as any[];
+            state[key] = values.map(
+                (value, index) => createRawState(value, Object.assign({}, state[key]?.[index]))
+            );
+            state[key]._errors = [];
+        } else if (typeof data[key] === 'object') {
+            state[key] = {
+                ...createRawState(data[key], Object.assign({}, state[key])),
+                _errors: [],
+            };
+        } else {
+            state[key] = {
+                _touched: state?.[key]?._touched || false,
+                _errors: [],
+            };
+        }
+    }
+
+    return state;
+}
+
+const validate = <Data = any>(form: Writable<Data>, schema: ObjectSchema<any>, state: Writable<FormState>, touchValid = false, touchInvalid = false) => {
+    try {
+        schema.validateSync(get(form), { abortEarly: false });
+        clearErrors(state, touchValid);
+        return true;
+    } catch (error) {
+        setErrors(state, error, touchValid, touchInvalid);
+        return false;
+    }
+}
+
+const clearErrors = (state: Writable<FormState> | any, touch = false, inner = false, update = true) => {
+    try {
+        let data = !inner ? get(state) : state;
+        for (let key in data) {
+            if (key !== '_touched' && key !== '_errors') {
+                if (Array.isArray(data[key])) {
+                    let values = (data[key] || []) as any[];
+                    data[key] = values.map(
+                        value => clearErrors(value, touch, true, update)
+                    );
+                    data[key]._touched = typeof data[key]?._touched === 'boolean' && !touch ? data[key]._touched : touch;
+                    data[key]._errors = [];
+                } else if (typeof data[key] === 'object') {
+                    data[key] = {
+                        ...clearErrors(data[key], touch, true, update),
+                        _touched: typeof data[key]?._touched === 'boolean' && !touch ? data[key]._touched : touch,
+                        _errors: [],
+                    };
+                } else {
+                    data[key] = {
+                        _touched: typeof data[key]?._touched === 'boolean' && !touch ? data[key]._touched : touch,
+                        _errors: [],
+                    };
+                }
+            }
+        }
+        if (!inner) {
+            update && state.set(data as FormState);
+            return data;
+        } else {
+            return data;
+        }
+    } catch (error) {
+        console.error(error);
+        return state;
+    }
+}
+
+const setErrors = (state: Writable<FormState>, mainError: ValidationError, touchValid = false, touchInvalid = false) => {
+    try {
+        let _state = get(state);
+        _state = clearErrors(_state, touchValid, true, false);
+        for (let error of mainError.inner) {
+            let segments = error.path.toString().match(/[^.[\]]+/g) || [];
+
+            let obj = _state;
+            if (segments.length > 1) {
+                const objSegments = segments.slice(0, segments.length - 1);
+                let exists = true;
+                for (let segment of objSegments) {
+                    if (!obj[segment]) {
+                        exists = false;
+                        break;
+                    }
+                    obj = obj[segment];
+                }
+                if (!exists) {
+                    break;
+                }
+            }
+
+            let lastSegment = segments[segments.length - 1];
+            if (!obj[lastSegment]) { obj[lastSegment] = { touched: false, _errors: [] }; }
+            if (!obj[lastSegment]._errors) { obj[lastSegment]._errors = []; }
+            obj[lastSegment]._errors.push(error.message);
+            obj[lastSegment]._touched = typeof obj[lastSegment]._touched === 'boolean' && !touchInvalid ? obj[lastSegment]._touched : touchInvalid;
+        }
+        state.set(_state);
+
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+const updateFieldTouched = (name: string, state: Writable<FormState>) => {
+    let segments = name.toString().match(/[^.[\]]+/g) || [];
+    let _state = get(state);
+    let obj = _state;
+    let exists = true;
+    if (segments.length > 1) {
+        const objSegments = segments.slice(0, segments.length - 1);
+        for (let segment of objSegments) {
+            if (!obj[segment]) {
+                exists = false;
+                break;
+            }
+            obj = obj[segment];
+        }
+    }
+
+    if (exists) {
+        let lastSegment = segments[segments.length - 1];
+
+        if (obj[lastSegment]) {
+            obj[lastSegment]._touched = true;
+        }
+    }
+    state.set(_state);
+}
+
+const updateState = (form: Writable<any>, validationSchema: ObjectSchema<any>, state: Writable<FormState>) => {
+    let _state = get(state);
+    if (validationSchema) {
+        _state = createValidatedState(get(form), validationSchema.fields, JSON.parse(JSON.stringify(_state)))
+    } else {
+        _state = createRawState(get(form), JSON.parse(JSON.stringify(_state)));
+    }
+    state.set(_state);
+}
+
+const getFieldState = (name: string, $state: FormState) => {
+    let segments = name.toString().match(/[^.[\]]+/g) || [];
+    let obj = $state;
+
+    const objSegments = segments.slice(0, segments.length - 1);
+    for (let segment of objSegments) {
+        if (!obj[segment]) {
+            return null;
+        }
+        obj = obj[segment];
+    }
+    let lastSegment = segments[segments.length - 1];
+    return obj[lastSegment] || null;
+}
+
+export function createForm<Data>({ initialValues, validationSchema, css: cssConfig, validateOnChange, validateOnBlur }: FormConfigInput<Data>) {
+    const values = writable<Data>(initialValues);
+    const state = writable<FormState>({});
+    const isValid = writable<boolean>(false);
+    const isTouched = writable<boolean>(false);
+    const css = {
+        ...{
+            enabled: true,
+            validClass: 'is-valid',
+            invalidClass: 'is-invalid',
+            useValid: true,
+            useInvalid: true,
+        }, ...(cssConfig || {})
+    };
+    validateOnChange = typeof validateOnChange !== 'boolean' ? true : validateOnChange;
+    validateOnBlur = typeof validateOnBlur !== 'boolean' ? true : validateOnBlur;
+    const cssValidator = writable<number>(0);
+    const validationReset = writable<number>(0);
+
+    createState(initialValues, state, validationSchema);
+
+    const updateForm = async () => {
+        updateState(values, validationSchema, state);
+        validateForm();
+    };
+
+    const validateForm = (highlight: 'none' | 'errors' | 'all' = 'none') => {
+        isValid.set(validate(values, validationSchema, state, highlight === 'all', highlight === 'all' || highlight === 'errors'));
+        css.enabled && !validateOnChange && highlight !== 'none' && cssValidator.update(val => val + 1);
+    }
+
+    const resetForm = (newValue?: Data) => {
+        if (newValue) {
+            values.set(newValue);
+        }
+        state.set({});
+        isValid.set(false);
+        isTouched.set(false);
+        createState(get(values), state, validationSchema);
+        validationReset.update(val => val + 1);
+    }
+
+    values.subscribe(() => {
+        validationSchema && validateForm();
+    });
+
+    const handleChange = (event: Event) => {
+        const target = event?.target as HTMLElement & { name: string };
+        target.name && updateFieldTouched(target.name, state);
+        isTouched.set(true);
+    }
+
+    const setTouched = (state: boolean) => {
+        isTouched.set(state);
+    }
+
+    const formControl = (node: HTMLElement & { name: string }, options: any = {}) => {
+        const changeListener = (event: Event) => {
+            if (validateOnChange) {
+                handleChange(event);
+                css.enabled && checkValidity(get(state));
+            }
+        }
+        const blurListener = (event: Event) => {
+            if (validateOnBlur) {
+                handleChange(event);
+                css.enabled && checkValidity(get(state));
+            }
+        }
+        let unsubscribeState: Unsubscriber = null;
+        let unsubscribeCssValidator: Unsubscriber = null;
+        let unsubscribeValidationReset: Unsubscriber = null;
+
+        const checkValidity = async ($state: FormState) => {
+            if (node.name) {
+                const fieldState = getFieldState(node.name, $state);
+                const invalid = fieldState?._touched && !!fieldState?._errors?.length;
+                const valid = fieldState?._touched && !fieldState?._errors?.length;
+                node.classList.remove(css.validClass);
+                node.classList.remove(css.invalidClass);
+                if (invalid) {
+                    css.useInvalid && node.classList.add(css.invalidClass);
+                } else if (valid) {
+                    css.useValid && node.classList.add(css.validClass);
+                }
+            }
+        }
+
+        if (['input', 'checkbox', 'radio', 'select', 'textarea'].includes(node.tagName)
+            || node.contentEditable) {
+            node.addEventListener('change', changeListener);
+            node.addEventListener('blur', blurListener);
+
+            if (node.name) {
+                unsubscribeState = state.subscribe($state => {
+                    css.enabled && validateOnChange && checkValidity($state);
+                });
+                unsubscribeCssValidator = cssValidator.subscribe(() => {
+                    checkValidity(get(state));
+                });
+                unsubscribeValidationReset = validationReset.subscribe(() => {
+                    node.classList.remove(css.validClass);
+                    node.classList.remove(css.invalidClass);
+                })
+            }
+        }
+        return {
+            destroy() {
+                node.removeEventListener('change', changeListener);
+                node.removeEventListener('blur', blurListener);
+                unsubscribeState && unsubscribeState();
+                unsubscribeCssValidator && unsubscribeCssValidator();
+                unsubscribeValidationReset && unsubscribeValidationReset();
+            }
+        }
+    }
+
+    return {
+        values,
+        state,
+        isValid,
+        isTouched,
+        validateForm,
+        handleChange,
+        updateForm,
+        setTouched,
+        formControl,
+        resetForm
+    };
+}
+
+export * as Schema from 'yup';
